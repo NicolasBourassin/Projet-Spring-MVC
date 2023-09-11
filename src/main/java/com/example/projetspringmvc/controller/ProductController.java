4acc4cbcd6a2c0e0045d2e18bec7f70ae055ee94
NicolasBourassin <bourassinnicolas@gmail.com>
2023-09-11 23:03:57 +0200

    deleting products OK. Should continue with update ...

--- src/main/java/com/example/projetspringmvc/controller/ProductController.java
+++ src/main/java/com/example/projetspringmvc/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.example.projetspringmvc.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -58,4 +59,14 @@ public class ProductController {
         return "redirect:/products";
     }
 
+    @PostMapping("/products/delete")
+    public String deleteProduct(@RequestParam("id") Long id) {
+        try {
+            productService.remove(id);
+            return "redirect:/products";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/products";
+        }
+    }
 }
