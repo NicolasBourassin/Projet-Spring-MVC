31c57233e7773c92c763dac9b5010066c500aa46
NicolasBourassin <bourassinnicolas@gmail.com>
2023-09-11 21:32:14 +0200

    before add-categories.html implementations.

--- src/main/java/com/example/projetspringmvc/controller/CategoryController.java
+++ src/main/java/com/example/projetspringmvc/controller/CategoryController.java
@@ -7,6 +7,7 @@ import com.example.projetspringmvc.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,6 +40,12 @@ public class CategoryController {
         List<Product> products = productService.findByCategory(category);
         model.addAttribute("products", products);
         return "products";
+    }
+
+    @PostMapping("/categories/delete/{categoryId}")
+    public String deleteCategoryAndProducts(@PathVariable Long categoryId) throws Exception {
+        categoryService.deleteCategoryAndProducts(categoryId);
+        return "redirect:/categories";
     }
 
 }
--- src/main/java/com/example/projetspringmvc/service/CategoryService.java
+++ src/main/java/com/example/projetspringmvc/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.example.projetspringmvc.service;
 
 import com.example.projetspringmvc.repository.CategoryRepository;
 import com.example.projetspringmvc.repository.entity.Category;
+import com.example.projetspringmvc.repository.entity.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +11,11 @@ import java.util.Optional;
 @Service
 public class CategoryService {
     private CategoryRepository categoryRepository;
-    public CategoryService(CategoryRepository categoryRepository){
+    private ProductService productService;
+    public CategoryService(CategoryRepository categoryRepository, ProductService productService){
         this.categoryRepository = categoryRepository;
+        this.productService = productService;
+
     }
 
     public Category fetchById(Long id) throws Exception{
@@ -31,4 +35,16 @@ public class CategoryService {
         categoryRepository.deleteById(id);
     }
 
+    public void deleteCategoryAndProducts(Long categoryId) throws Exception {
+        Category category = fetchById(categoryId);
+        List<Product> products = productService.findByCategory(category);
+
+        // We must delete Product instances before the Category
+        for (Product product : products) {
+            productService.remove(product.getId());
+        }
+
+        // Delete the category
+        remove(categoryId);
+    }
 }
